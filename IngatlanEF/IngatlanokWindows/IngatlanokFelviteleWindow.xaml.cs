5fbe33af9368b79a1e120ecd379156dadf66f859
Laci <1sikelaszlo@gmail.com>
2024-12-10 11:28:16 +0100

    Add project files.

---
+++ IngatlanEF/IngatlanokWindows/IngatlanokFelviteleWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using IngatlanEF.Models;
+using IngatlanEF.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IngatlanEF.IngatlanokWindows
+{
+    /// <summary>
+    /// Interaction logic for IngatlanokFelviteleWindow.xaml
+    /// </summary>
+    public partial class IngatlanokFelviteleWindow : Window
+    {
+        public IngatlanokFelviteleWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMentes_Click(object sender, RoutedEventArgs e)
+        {
+            Ingatlan ujIngatlan = new()
+            {
+                Id = 0,
+                Település = tbxTelepules.Text,
+                Cim = tbxCim.Text,
+                Ár = int.Parse(tbxAr.Text),
+                Tipus = cbxTipus.Text,
+                KepNev = tbxKepnev.Text,
+                UgyintezoId = int.Parse(cbxUgyintezo.Text)
+            };
+            IngatlanService.IngatlanInsert(ujIngatlan);
+            MessageBox.Show("Sikeres rögzítés.");
+        }
+
+        private void btnMegsem_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ IngatlanEF/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using IngatlanEF.IngatlanokWindows;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IngatlanEF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public const string JELSZO = "Asgragh";
+        public static bool isLogged = false;
+        public static string logName = "";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LogInOut(object sender, RoutedEventArgs e)
+        {
+            if (isLogged)
+            {
+                lblBejelentkezve.Content = "Bejelentkezve:";
+                isLogged = false;
+                menuBelepes.Header = "Belépés";
+                menuIngatlanok.IsEnabled = false;
+                menuUgyintezok.IsEnabled = false;
+            }
+            else
+            {
+                //megnyitok egy új ablakot, ott bekérem a jelszót és a felhasználónevet
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.ShowDialog();
+                if (isLogged)
+                {
+                    //ha az helyes, akkor 
+                    //aktiválom a másik két menüpontot
+                    menuIngatlanok.IsEnabled = true;
+                    menuUgyintezok.IsEnabled = true;
+                    //kiíratom a felhasználónevet
+                    lblBejelentkezve.Content = $"Bejelentkezve: {logName}";
+                    menuBelepes.Header = "Kilépés";
+                }
+            }
+        }
+
+        private void IngatlanokListaja(object sender, RoutedEventArgs e)
+        {
+            IngatlanListaWindow ingatlanListaWindow = new IngatlanListaWindow();
+            ingatlanListaWindow.ShowDialog();
+        }
+
+        private void IngatlanokFelvitele(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
